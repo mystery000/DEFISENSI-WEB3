01e588008b53c7cb4e8f0ea99e78be837b16f081
arthlo <lkostyrka17@gmail.com>
2023-09-14 15:54:12 +0200

    Arbitrum: Get transactions by NFT collection

--- backend/src/arbitrum/arbitrum.service.ts
+++ backend/src/arbitrum/arbitrum.service.ts
@@ -6,7 +6,7 @@ import Moralis from 'moralis';
 import { NetworkType } from 'src/utils/enums/network.enum';
 import { TransactionType } from 'src/utils/enums/transaction.enum';
 import { logger } from 'src/utils/logger';
-import { ChainbaseChain, HistoricalPrice, NFTTransaction, TokenTransaction } from 'src/utils/types';
+import { Action, ChainbaseChain, HistoricalPrice, NFTTransaction, TokenTransaction } from 'src/utils/types';
 
 @Injectable()
 export class ArbitrumService {
@@ -91,8 +91,228 @@ export class ArbitrumService {
   }
 
   async getTransactionsByNFT(address: string, fromBlock: number = 0) {
-    const transactions: NFTTransaction[] = [];
-    return transactions;
+    let txHashs = [];
+    let transactions: NFTTransaction[] = [];
+    try {
+      // Get NFT transfers by contract and extract transaction hashs
+      let transfers = [];
+
+      await Moralis.EvmApi.nft
+        .getNFTContractTransfers({
+          chain: EvmChain.ARBITRUM,
+          format: 'decimal',
+          address: address,
+          limit: 4,
+          fromBlock,
+        })
+        .then((response) => {
+          transfers = response.toJSON().result;
+        });
+
+      for (const transfer of transfers) {
+        if (!transfer.possible_spam && !txHashs.includes(transfer.transaction_hash)) {
+          txHashs.push(transfer.transaction_hash);
+        }
+      }
+      console.log(txHashs);
+      // Get decoded transaction by hash
+      for (const txHash of txHashs) {
+        const transaction = await Moralis.EvmApi.transaction.getTransactionVerbose({
+          chain: EvmChain.ARBITRUM,
+          transactionHash: txHash,
+        });
+
+        const { logs, block_number, block_timestamp, from_address, to_address } = transaction.toJSON();
+        // Check if there is OrderFulFilled event in logs
+        const order = logs.find((log) => log?.decoded_event?.label === 'OrderFulfilled');
+        if (order) {
+          let metadata: any = {};
+
+          /* Metadata Type:
+              sales?: number,
+              purchases?: number,
+              name: string, 
+              symbol: string 
+              contractType: 'ERC721' | 'ERC1155'
+          */
+
+          for (const log of logs) {
+            if (log.decoded_event?.label === 'OrderFulfilled') {
+              // Check if order type is purchase or sale
+              const offer = log.decoded_event.params.find((param) => param.name === 'offer')?.value || '';
+              const decodedValue = offer.split(',');
+              const spentItems: { itemType: number; address: string; identifier: string; amount: number }[] = [];
+              for (let i = 0; i < decodedValue.length; i += 4) {
+                if (decodedValue[i] === undefined) return;
+                spentItems.push({
+                  itemType: Number(decodedValue[i]),
+                  address: decodedValue[i + 1],
+                  identifier: decodedValue[i + 2],
+                  amount: Number(decodedValue[i + 3]),
+                });
+              }
+              for (const item of spentItems) {
+                /*
+                 itemType 0: Native Token
+                          1: ERC20
+                          2: ERC721
+                */
+                if (item.itemType == 2 && metadata[item.address]) {
+                  metadata[item.address] = {
+                    ...metadata[item.address],
+                    sales: metadata[item.address].sales + item.amount,
+                  };
+                }
+
+                if (item.itemType === 2 && !metadata[item.address]) {
+                  await Moralis.EvmApi.nft
+                    .getNFTContractMetadata({
+                      chain: EvmChain.ARBITRUM,
+                      address: item.address,
+                    })
+                    .then((response) => {
+                      metadata[item.address] = { ...response.toJSON(), sales: item.amount };
+                    })
+                    .catch((error) => {});
+                }
+              }
+            }
+          }
+
+          let actions: Action[] = [];
+          Object.entries(metadata).map(([key, value]: [key: string, value: any]) => {
+            if (value.sales) {
+              actions.push({
+                type: 'Sale',
+                amount: value.sales,
+                tokenAddress: key,
+                name: value.name,
+                symbol: value.symbol,
+              });
+            }
+            if (value.purchases) {
+              actions.push({
+                type: 'Purchase',
+                amount: value.purchases,
+                tokenAddress: key,
+                name: value.name,
+                symbol: value.symbol,
+              });
+            }
+          });
+
+          if (actions.length) {
+            transactions.push({
+              txHash,
+              blockNumber: block_number,
+              type: TransactionType.NFT,
+              network: NetworkType.ARBITRUM,
+              timestamp: new Date(block_timestamp).getTime(),
+              details: {
+                from: from_address,
+                to: to_address,
+                actions,
+              },
+            });
+          }
+        } else {
+          let metadata: any = {};
+          /* Metadata Type:
+
+              mints?: number
+              transfers?: number
+              burns?: number
+              name: string,
+              symbol: string
+              contractType: 'ERC721' | 'ERC1155'
+          */
+
+          // Tranfer
+          for (const log of logs) {
+            if (!metadata[log.address]) {
+              await Moralis.EvmApi.nft
+                .getNFTContractMetadata({
+                  chain: EvmChain.ARBITRUM,
+                  address: log.address,
+                })
+                .then((response) => {
+                  metadata[log.address] = { ...response.toJSON() };
+                })
+                .catch((error) => {});
+            }
+            // Parse ERC721 and ERC1155
+            if (metadata[log.address]) {
+              if (log.decoded_event?.label === 'Transfer') {
+                const from = log.decoded_event.params.find((param) => param.name === 'from').value;
+                const to = log.decoded_event.params.find((param) => param.name === 'to').value;
+                if (Number(from) === 0) {
+                  // Mint Tranfer
+                  metadata[log.address] = { ...metadata[log.address], mints: (metadata[log.address].mints || 0) + 1 };
+                } else if (Number(to) === 0) {
+                  // Burn Transfer
+                  metadata[log.address] = { ...metadata[log.address], burns: (metadata[log.address].burns || 0) + 1 };
+                } else {
+                  metadata[log.address] = {
+                    ...metadata[log.address],
+                    transfers: (metadata[log.address].transfers || 0) + 1,
+                  };
+                }
+              }
+            }
+          }
+
+          let actions: Action[] = [];
+          Object.entries(metadata).map(([key, value]: [key: string, value: any]) => {
+            if (value.mints) {
+              actions.push({
+                type: 'Mint',
+                amount: value.mints,
+                tokenAddress: key,
+                name: value.name,
+                symbol: value.symbol,
+              });
+            }
+            if (value.burns) {
+              actions.push({
+                type: 'Burn',
+                amount: value.burns,
+                tokenAddress: key,
+                name: value.name,
+                symbol: value.symbol,
+              });
+            }
+            if (value.transfers) {
+              actions.push({
+                type: 'Transfer',
+                amount: value.transfers,
+                tokenAddress: key,
+                name: value.name,
+                symbol: value.symbol,
+              });
+            }
+          });
+
+          if (actions.length > 0) {
+            transactions.push({
+              txHash,
+              blockNumber: block_number,
+              type: TransactionType.NFT,
+              network: NetworkType.ARBITRUM,
+              timestamp: new Date(block_timestamp).getTime(),
+              details: {
+                from: from_address,
+                to: to_address,
+                actions,
+              },
+            });
+          }
+        }
+      }
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      return transactions;
+    }
   }
 
   // Get the price history of ERC20 token for 90 days
@@ -117,6 +337,6 @@ export class ArbitrumService {
   }
 
   async test() {
-    return this.getTransactionsByContract('0xda10009cbd5d07dd0cecc66161fc93d7c9000da1');
+    return this.getTransactionsByNFT('0xC36442b4a4522E871399CD717aBDD847Ab11FE88');
   }
 }
